f18a95332631aa2a6ca0f37492b31dec9d4fb600
Sean Patrick Hagen <shagen@bardel.ca>
2016-08-17 16:29:29 -0700

    Change SQL template

--- lib/goose/migrate.go
+++ lib/goose/migrate.go
@@ -404,10 +404,13 @@ func Down_{{ . }}(txn *sql.Tx) {
 
 var sqlMigrationTemplate = template.Must(template.New("goose.sql-migration").Parse(`
 -- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
+-- +goose StatementBegin
+
+-- +goose StatementEnd
 
 
 -- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
+-- +goose StatementBegin
 
+-- +goose StatementEnd
 `))
